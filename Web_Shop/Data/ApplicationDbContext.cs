dce9bfd088cda22b61be8e3725c03a3f1e3eff23
rodr5000 <roke2309@gmail.com>
2025-10-22 16:48:47 +0300

    Add project files.

---
+++ Web_Shop/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web_Shop.Models;
+
+
+namespace ShopSbS.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Add DbSet<TEntity> properties here, for example:
+        // public DbSet<YourEntity> YourEntities { get; set; }
+    }
+}
---
+++ Web_Shop/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web_Shop.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public double Price { get; set; } = 0.0;
+        public string Description { get; set; } = string.Empty;
+        public string ImageUrl {  get; set; } = string.Empty;
+    }
+}
